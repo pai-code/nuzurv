36683f32ada1266ec803d781508e8f3fb83d639e
pai-code <paitoucher@gmail.com>
2019-10-30 10:19:07 -0700

    Updates

---
+++ precache-manifest.f61f6a723e6acf6a21f0e134773be13f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839b2382aa78421526d2c91c4a905d6d",
+    "url": "/nuzurv/index.html"
+  },
+  {
+    "revision": "d88d98fbae672bbde37c",
+    "url": "/nuzurv/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5b2564f7135b69c8a99c",
+    "url": "/nuzurv/static/js/2.404476bd.chunk.js"
+  },
+  {
+    "revision": "d88d98fbae672bbde37c",
+    "url": "/nuzurv/static/js/main.77898ee4.chunk.js"
+  },
+  {
+    "revision": "efb77b69335ae662b660",
+    "url": "/nuzurv/static/js/runtime-main.9316fe0e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/nuzurv/static/media/logo.25bf045c.svg"
+  }
+]);
